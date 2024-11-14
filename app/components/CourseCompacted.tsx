f9685ab786c2a4f2a920c2f3580cca7097796e4b
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-11-14 18:43:27 -0500

    started main page

---
+++ app/components/CourseCompacted.tsx
@@ -0,0 +1,11 @@
+export default function CourseCompacted() {
+    return (
+        <div className="flex justify-between items-center w-80 p-6 bg-white shadow-md rounded-lg">
+            <div>
+                <p className="font-bold text-xl text-gray-700">CLASS XXX</p>
+                <p className="font-bold text-lg text-gray-400">XX</p>
+            </div>
+            <p className="font-semibold text-5xl">X.XX</p>
+        </div>
+    )
+}
